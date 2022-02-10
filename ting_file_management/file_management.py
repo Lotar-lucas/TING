ed42d30f7a3a6fa08787e791018e9b1fdf86ee58
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-02-09 22:19:01 -0300

    Done two requirement

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,26 @@
+# import sys
+
+
+# def txt_importer(path_file):
+#     if path_file.endswith(".txt"):
+#         return sys.stderr.write("Formato inválido\n")
+
+#
+
+import os
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    extension = os.path.splitext(path_file)
+    if extension[1] != ".txt":
+        return sys.stderr.write("Formato inválido\n")
+
+    # Fui auxiliado por Felippe Correa
+    try:
+        data = open(path_file)
+        with data as data_content:
+            return data_content.read().splitlines()
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
