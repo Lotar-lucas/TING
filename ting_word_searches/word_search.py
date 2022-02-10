33cbf21d3b866d7c4e6c05e126bd18a29382152b
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-02-09 23:26:58 -0300

    Done six requirement

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
+# fui auxiliado por Felippe Correa
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = list()
+
+    for file in instance.dataFiles:
+        word_occurrence = []
+
+        for str_word in file["linhas_do_arquivo"]:
+
+            if word.lower() in str_word.lower():
+                word_occurrence.append({"linha": str_word.count(word)})
+
+                response.append(
+                    {
+                        "palavra": word,
+                        "arquivo": file["nome_do_arquivo"],
+                        "ocorrencias": word_occurrence,
+                    }
+                )
+    return response
 
 
 def search_by_word(word, instance):
